3ead532c870e9a07b650f30a5a1e73093148c683
Alla Ponurko <aponurko@gmail.com>
2022-11-26 02:44:49 +0300

    231

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -43,5 +43,20 @@ namespace WebApplication1.Controllers
         {
             return View("About_me");
         }
+        //[HttpGet]
+        //public async Task Index()
+        //{
+        //    string content = @"<form method='post'>
+        //        <label>Name:</label><br />
+        //        <input name='name' /><br />
+        //        <label>Age:</label><br />
+        //        <input type='number' name='age' /><br />
+        //        <input type='submit' value='Send' />
+        //    </form>";
+        //    Response.ContentType = "text/html;charset=utf-8";
+        //    await Response.WriteAsync(content);
+        //}
+        //[HttpPost]
+        //public string Index(string name, int age) => $"{name}: {age}";
     }
 }
